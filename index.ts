8f384018e9a5526af9865c4719ac736a73f7781d
vorn-tovtrip <sovanpanhavorn.siv@tovtrip.com>
2025-05-23 21:39:17 +0700

    s

--- index.ts
+++ index.ts
@@ -15,6 +15,6 @@ PostgresDataSource.initialize().then(() => {
     console.log("Express js server is up running");
   });
   server.on("close", async () => {
-    console.log("Express js server is shutting downs");
+    console.log("Express js servers is shutting downs");
   });
 });
